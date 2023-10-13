edb33290097d7cc05baea956dee8459d92a7e7fb
Ivan Paiva <stelsonip@gmail.com>
2023-10-13 16:08:23 +0200

    Updating tasks

--- src/main/java/com/ivan/todolist/filter/FilterTaskAuth.java
+++ src/main/java/com/ivan/todolist/filter/FilterTaskAuth.java
@@ -17,7 +17,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 // 
 @Component
-public class FilterTaskAuth extends OncePerRequestFilter{
+public class FilterTaskAuth extends OncePerRequestFilter {
 
     @Autowired
     private IUserRepository userRepository;
@@ -26,38 +26,50 @@ public class FilterTaskAuth extends OncePerRequestFilter{
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-                // get auth (user & password)
-                var authorization = request.getHeader("Authorization");
-                
-                // substring method used to extract a part of a text (in this case to separate the *Basic* from the Base64 generated
-                var authEncoded = authorization.substring("Basic".length()).trim();
+        // validation should only be in the /tasks path/route
 
-                // gets the enconded Base64 and turns into an array of bytes
-                byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+        var servletPath = request.getServletPath();
 
-                // gets the array of bytes and turns into string
-                var authString = new String(authDecode);
+        if (servletPath.equals("/tasks/")) {
+            // get auth (user & password)
+            var authorization = request.getHeader("Authorization");
 
-                //will return an array spliting the username and password which were 
-                //username:password => will now be ["username", "password"]
-                String[] credentials = authString.split(":");
-                String username = credentials[0];
-                String password = credentials[1];
+            // substring method used to extract a part of a text (in this case to separate
+            // the *Basic* from the Base64 generated
+            var authEncoded = authorization.substring("Basic".length()).trim();
 
-                // validate users existence
-                var user = this.userRepository.findByUsername(username);
-                if (user == null) {
-                    response.sendError(401);
-                } else {
-                    // validate password
-                    var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
-                    if(passwordVerify.verified) {
+            // gets the enconded Base64 and turns into an array of bytes
+            byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+            // gets the array of bytes and turns into string
+            var authString = new String(authDecode);
+
+            // will return an array spliting the username and password which were
+            // username:password => will now be ["username", "password"]
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
+
+            // validate users existence
+            var user = this.userRepository.findByUsername(username);
+            if (user == null) {
+                response.sendError(401);
+            } else {
+                // validate password
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                if (passwordVerify.verified) {
+                    // sending an attribute inside the controller
+                    request.setAttribute("idUser", user.getId());
                     filterChain.doFilter(request, response);
-                    }else {
-                        response.sendError(401);
-                    }
-                    
+                } else {
+                    response.sendError(401);
                 }
-                
+            }
+            
+        } else {
+            filterChain.doFilter(request, response);
+        }
+
+        
     }
 }
--- src/main/java/com/ivan/todolist/task/TaskController.java
+++ src/main/java/com/ivan/todolist/task/TaskController.java
@@ -1,10 +1,19 @@
 package com.ivan.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,9 +23,22 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
+
+        // checking of the date we're validating isn't bigger than the current
+        // current time must be bigger than starting time
+
         var task = this.taskRepository.save(taskModel);
-        return task;
+        return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+        return tasks;
     }
     
 }
